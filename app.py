4cf3d78a7a53412232a6de29df730a0ce316302c
lucasboos <lucasbooslb@gmail.com>
2023-04-01 10:04:58 -0300

    User route

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
+from controller.users import User, Users
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,6 +21,9 @@ app.config["JWT_SECRET_KEY"] = "Senai2022"
 @app.before_first_request
 def create_database():
     database.create_all()
+
+api.add_resource(Users, '/users')
+api.add_resource(User, '/users/<int:id>', '/users')
 
 
 if __name__ == '__main__':
--- models/user.py
+++ models/user.py
@@ -1,6 +1,4 @@
 from sql_alchemy import database
-from sqlalchemy.sql.expression import func
-import datetime
 
 
 class UserModel(database.Model):
@@ -8,8 +6,8 @@ class UserModel(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     email = database.Column(database.String(50))
     password = database.Column(database.String(255))
-    created_at = database.Column(database.DateTime, server_default=func.sysdate())
-    updated_at = database.Column(database.DateTime, onupdate=datetime.datetime.now)
+    created_at = database.Column(database.DateTime, server_default=database.func.now())
+    updated_at = database.Column(database.DateTime, server_default=database.func.now(), server_onupdate=database.func.now())
 
     def __init__(self, id, email, password):
         self.id = id
@@ -37,6 +35,13 @@ class UserModel(database.Model):
         if user:
             return user
         return None
+
+    @classmethod
+    def find_last_user(cls):
+        user_id = database.session.query(cls).order_by(cls.id.desc()).first()
+        if user_id:
+            return user_id.id + 1
+        return 1
 
     def save_user(self):
         database.session.add(self)
